d03ef3ccbfd92f7c908026e01ef89d7bd28b7c05
Jakub Psarski <psarski@student.agh.edu.pl>
2025-01-21 23:07:38 +0100

    fixed product routes

--- server/models/product.js
+++ server/models/product.js
@@ -1,39 +1,48 @@
 import { DataTypes } from "sequelize";
 import { database } from "../database/database.js";
 
-const Product = database.define("Product", {
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
+const Product = database.define(
+	"Product",
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.FLOAT,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+		category: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		rating: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		count: {
+			type: DataTypes.FLOAT,
+			allowNull: true,
+		},
+		stock: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
 	},
-	title: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	price: {
-		type: DataTypes.FLOAT,
-		allowNull: false,
-	},
-	description: {
-		type: DataTypes.STRING,
-		allowNull: true,
-	},
-	category: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	image: {
-		type: DataTypes.STRING,
-		allowNull: false,
-	},
-	rating: {
-		type: DataTypes.FLOAT,
-		allowNull: true,
-	},
-	count: {
-		type: DataTypes.FLOAT,
-		allowNull: true,
-	},
-});
+	{ timestamps: false }
+);
 
 export default Product;
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,37 +1,34 @@
+/*
+todo:
+add get route for reviews?
+*/
+
 import express from "express";
+import { database } from "../database/database.js";
 import Product from "../models/product.js";
 
 const router = express.Router();
 
-router.get("/", async (_, res) => {
-	const products = await Product.findAll();
-	res.status(200).json(products);
-});
-
-router.get("/:id", async (req, res) => {
-	const product = await Product.findByPk(req.params.id);
-	if (!product) res.status(404).send("Product not found");
-	else res.status(200).json(product);
-});
-
 router.post("/", async (req, res) => {
 	try {
-        const {
-            id,
-            title,
-            price,
-            description,
-            category,
-            image,
-            rating
-        } = req.body;
-		if (!title || !price || !category || !image) {
-			return res
-				.status(400)
-				.json({ error: "Required fields are missing" });
+		const { title, price, description, category, image, rating, stock } =
+			req.body;
+		if (!title || !price || !category || !image || !stock) {
+			return res.status(400).json({
+				error: `Required fields are missing: ${
+					!title
+						? "title"
+						: !price
+						? "price"
+						: !category
+						? "category"
+						: !image
+						? "image"
+						: "stock"
+				}`,
+			});
 		}
 		const newProduct = await Product.create({
-			id,
 			title,
 			price,
 			description,
@@ -39,6 +36,7 @@ router.post("/", async (req, res) => {
 			image,
 			rating: rating ? rating.rate : null,
 			count: rating ? rating.count : null,
+			stock,
 		});
 		res.status(201).json({ id: newProduct.id });
 	} catch (error) {
@@ -46,13 +44,58 @@ router.post("/", async (req, res) => {
 	}
 });
 
-router.delete("/:id", async (req, res) => {
+router.patch("/id/:id", async (req, res) => {
+	const product = await Product.findByPk(req.params.id);
+	if (!product) res.status(404).send("Product not found");
+	else {
+		const { title, price, description, category, image, rating, stock } =
+			req.body;
+		if (!title && !price && !description && !category && !image && !stock) {
+			return res.status(400).send("No fields to update");
+		}
+		await product.update({
+			title: title ? title : product.title,
+			price: price ? price : product.price,
+			description: description ? description : product.description,
+			category: category ? category : product.category,
+			image: image ? image : product.image,
+			rating: rating ? rating.rate : product.rating,
+			count: rating ? rating.count : product.count,
+			stock: stock ? stock : product.stock,
+		});
+		res.status(200).send("Product updated");
+	}
+});
+
+router.delete("/id/:id", async (req, res) => {
 	const product = await Product.findByPk(req.params.id);
 	if (!product) res.status(404).send("Product not found");
 	else {
 		await product.destroy();
+		await database.query(
+			"DELETE FROM sqlite_sequence WHERE name='Products'"
+		);
 		res.status(200).send("Product deleted");
 	}
+});
+
+router.get("/id/:id", async (req, res) => {
+	const product = await Product.findByPk(req.params.id);
+	if (!product) res.status(404).send("Product not found");
+	else res.status(200).json(product);
+});
+
+router.get("/", async (_, res) => {
+	const products = await Product.findAll();
+	res.status(200).json(products);
+});
+
+router.get("/categories", async (_, res) => {
+	const categories = await Product.findAll({
+        attributes: ["category"],
+        group: ["category"],
+    });
+	res.status(200).json(categories);
 });
 
 export default router;
